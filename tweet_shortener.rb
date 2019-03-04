8b99617ca7f6b13a5c47f82201cad4794044a338
Daniel Schneider <daschne8@gmail.com>
2019-03-04 16:47:47 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def dictionary(array)
+  substitutes = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    'be' => 'b',
+    'you'=> 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
