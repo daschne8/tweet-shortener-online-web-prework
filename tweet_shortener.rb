6a961f8f0ccd2dd9098e3644a187c1168ecf1d95
Daniel Schneider <daschne8@gmail.com>
2019-03-04 16:56:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,10 +16,12 @@ def word_substituter(tweet)
   tweet_array = tweet.split
   substitutes = dictionary
   keys = substitutes.keys
+  new_array = []
   tweet_array.collect do |word|
     if keys.include?(word)
       word = substitutes[word]
     end
+    new_array << word
   end
-  return tweet_array.join(" ")
+  return new_array.join(" ")
 end
